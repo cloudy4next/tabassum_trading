34d7444b8a9a6b86a472e451a1e3829f0c9bdb86
jahangir <jahangir7200@live.com>
2022-04-09 16:41:23 +0600

    fix: typos

--- app/Http/Controllers/Admin/GrammenphoneDailyUpfrontCrudController.php
+++ app/Http/Controllers/Admin/GrammenphoneDailyUpfrontCrudController.php
@@ -16,7 +16,7 @@ class GrammenphoneDailyUpfrontCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
     // use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
     // use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    // use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     // use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
     /**
